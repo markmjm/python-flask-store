114ae67524a51d2cf270044fb30dccd9bd9afe78
markmjm <markmnei@gmail.com>
2018-04-15 16:16:52 -0400

    initial push

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # creates new end point /auth
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank")
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        # check if item exists
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An with name '{}' already exists.".format(name)}, 400
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': "Item {} deleted".format(name)}, 200
+
+    def put(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        data = Item.parser.parse_args()
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}, 200
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=9090, debug=True)
